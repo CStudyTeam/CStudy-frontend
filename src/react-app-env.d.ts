d7a4aec745a932056c2fe605a2792d72428d084d
seungmin <magin0auto@gmail.com>
2023-06-08 21:30:47 +0900

    :lipstick:design: 3D 모델링 추가 및 eslint, ts 규칙 추가

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="react-scripts" />
+
+import { UnrealBloomPass } from 'three-stdlib';
+
+declare global {
+    namespace JSX {
+        interface IntrinsicElements {
+            unrealBloomPass: ReactThreeFiber.Node<UnrealBloomPass, typeof UnrealBloomPass>;
+        }
+    }
+}
