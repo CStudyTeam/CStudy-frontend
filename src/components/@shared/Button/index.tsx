33487e45690f5777b08ed75f2b11d5a232cf1208
seriparkdev <qkrtpfl9228@gmail.com>
2023-06-10 02:19:01 +0900

    :lipstick:design: 문제집 페이지 퍼블리싱 (#4)

--- src/components/@shared/Button/index.tsx
+++ src/components/@shared/Button/index.tsx
@@ -1,7 +1,41 @@
-import React, { PropsWithChildren } from 'react';
+import { ButtonHTMLAttributes } from 'react';
+import * as Styled from './style';
 
-const Button = ({ children }: PropsWithChildren) => {
-    return <button>{children}</button>;
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    type?: 'button' | 'submit' | 'reset';
+    width?: string;
+    backgroundColor?: string;
+    color?: string;
+    fontSize?: string;
+    main?: boolean;
+    margin?: string;
+}
+
+const Button = ({
+    children,
+    type = 'button',
+    main,
+    width,
+    backgroundColor,
+    color,
+    fontSize,
+    margin,
+    ...rest
+}: ButtonProps) => {
+    return (
+        <Styled.Button
+            type={type}
+            main={main}
+            width={width}
+            backgroundColor={backgroundColor}
+            color={color}
+            fontSize={fontSize}
+            margin={margin}
+            {...rest}
+        >
+            {children}
+        </Styled.Button>
+    );
 };
 
 export default Button;
--- src/pages/Workbook/index.tsx
+++ src/pages/Workbook/index.tsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import Button from 'components/@shared/Button';
+import * as Styled from './style';
+import { COLOR } from 'constants/Color';
 
 const Workbook = () => {
-    return <div>문제집</div>;
+    return (
+        <>
+            <Styled.Head>
+                <Styled.PageTitle>문제집</Styled.PageTitle>
+                <div>
+                    <Styled.SearchInput type="text" />
+                    <Button width="5rem" backgroundColor={COLOR.NAVY_200} color="#eee">
+                        검색
+                    </Button>
+                </div>
+            </Styled.Head>
+            <Styled.WorkBookCards>
+                <Styled.WorkBookCard>
+                    <Styled.Title>타이틀</Styled.Title>
+                    <Styled.Time>2022.02.03</Styled.Time>
+                    <Styled.Img>이미지</Styled.Img>
+                    <Styled.Description>설명설명설명설명설명설명설명설명설명설명설명설</Styled.Description>
+                </Styled.WorkBookCard>
+            </Styled.WorkBookCards>
+        </>
+    );
 };
 
 export default Workbook;
