f9293115445f47e1005e2569857443c1231c5080
seriparkdev <qkrtpfl9228@gmail.com>
2023-06-24 01:30:20 +0900

    :sparkles:feat: 403 error가 발생했을 때 처리 로직 추가  (#1)

--- src/api/index.ts
+++ src/api/index.ts
@@ -54,6 +54,9 @@ instance.interceptors.response.use(
                 // 로그아웃
             }
         }
+        if (error.status === 403) {
+            alert('접근 권한이 없습니다.');
+        }
         throw new Error(error);
     },
 );
