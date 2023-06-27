f7d6415b1afb3647ddcea87494cc6350633d48ca
seungmin <magin0auto@gmail.com>
2023-06-27 15:23:41 +0900

    :sparkles:feat: admin 문제집 생성 api 추가 (#9)

--- src/api/workbookList.ts
+++ src/api/workbookList.ts
@@ -1,7 +1,14 @@
 import { instance } from 'api';
+import { WorkbookSetForm } from 'types/Form';
 import { Workbook } from 'types/workbookList';
 
 export const getWorkbookList = async () => {
     const response = await instance.get<Workbook[]>('/api/workbook/list');
+    return response.data;
+};
+
+export const workbookSet = async (formData: WorkbookSetForm) => {
+    const response = await instance.post('/api/workbook/create', formData);
+
     return response.data;
 };
