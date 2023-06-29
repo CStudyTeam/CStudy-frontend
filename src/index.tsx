4a4e6ec598d9c9636f7b44158cda8e76951b4dbc
seungmin <magin0auto@gmail.com>
2023-06-29 14:16:01 +0900

    :lipstick:design: react-calendar css 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './reset.css';
 import App from './App';
+import './reset.css';
+import 'react-calendar/dist/Calendar.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
