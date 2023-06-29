7603a33b55da6481242fc492bf6ddace9b2805ad
seungmin <magin0auto@gmail.com>
2023-06-29 14:37:47 +0900

    :sparkles:feat: 페이지네이션 기능 연결 (#4)

--- src/hooks/@query/workbookList/useGetWorkbookList.ts
+++ src/hooks/@query/workbookList/useGetWorkbookList.ts
@@ -2,8 +2,12 @@ import { useQuery } from '@tanstack/react-query';
 import { getWorkbookList } from 'api/workbookList';
 import { Workbook } from 'types/workbookList';
 
-const useGetWorkbookList = () => {
-    const { data: workbookList } = useQuery<Workbook[]>(['woorkbookList'], getWorkbookList);
+interface UseGetWorkbookListProps {
+    page: number;
+}
+
+const useGetWorkbookList = ({ page }: UseGetWorkbookListProps) => {
+    const { data: workbookList } = useQuery<Workbook>(['woorkbookList', { page }], () => getWorkbookList({ page }));
     return workbookList;
 };
 
