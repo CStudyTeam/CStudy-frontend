984b3d677c26502ad91df8207b61bd3fb00cf221
seungmin <magin0auto@gmail.com>
2023-06-30 16:59:58 +0900

    :lipstick:design: 공통 버튼 스타일 추가

--- src/components/@shared/Button/style.ts
+++ src/components/@shared/Button/style.ts
@@ -1,15 +1,17 @@
 import styled from 'styled-components';
 
 import { ButtonProps } from '.';
+import { SHADOW } from 'constants/Shadow';
 
 export const Button = styled.button<ButtonProps>`
     display: inline-block;
     width: ${(props) => props.width};
     margin: ${(props) => props.margin};
     padding: ${(props) => (props.main ? '1.5rem 0' : '1.3rem 0')};
-    border-radius: ${(props) => props.borderRadius};
     background-color: ${(props) => props.backgroundColor};
     color: ${(props) => props.color};
     font-size: ${(props) => props.fontSize};
     font-weight: ${(props) => props.weight};
+    border-radius: ${(props) => props.borderRadius};
+    box-shadow: ${(props) => props.shadow && SHADOW.CONTENTBOX_MD};
 `;
