c6d9c9dd9a62a6f02808656507b889f3f2584489
seungmin <magin0auto@gmail.com>
2023-06-30 20:09:23 +0900

    :art:style: problem api 코드 정리(Http 메서드끼리 묶음)

--- src/api/problem.ts
+++ src/api/problem.ts
@@ -1,6 +1,26 @@
 import { instance } from 'api';
 import { QuestionDataProps } from 'types/problemForm';
 
+/* -------- Get 요청 -------- */
+
+//  전체 문제 페이징
+export const getProblem = async ({ categoryTitle = '', questionSuccess = -1, page = 0, size = 10 }) => {
+    const response = await instance.get(
+        `/api/questions?categoryTitle=${categoryTitle}&questionSuccess=${questionSuccess}&page=${page}&size=${size}`,
+    );
+
+    return response.data;
+};
+
+//  단일 문제 찾기
+export const getOneProblem = async (problemId: string) => {
+    const response = await instance.get(`/api/questions/${problemId}`);
+    return response.data;
+};
+
+/* -------- POST 요청 -------- */
+
+// 문제 생성하기 & 대량 문제 생성하기
 export const problemSet = async (formData: QuestionDataProps[]) => {
     if (formData.length === 1) {
         const response = await instance.post('/api/question', formData[0]);
@@ -15,10 +35,8 @@ export const problemSet = async (formData: QuestionDataProps[]) => {
     throw new Error('문제 생성에 실패했습니다.');
 };
 
-export const getProblem = async ({ categoryTitle = '', questionSuccess = -1, page = 0, size = 10 }) => {
-    const response = await instance.get(
-        `/api/questions?categoryTitle=${categoryTitle}&questionSuccess=${questionSuccess}&page=${page}&size=${size}`,
-    );
-
-    return response.data;
+// 단일 문제 정답 선택하기
+export const SelectAnswerProblem = async (problemId: string) => {
+    const response = await instance.post(`/api/questions/${problemId}`);
+    return response;
 };
