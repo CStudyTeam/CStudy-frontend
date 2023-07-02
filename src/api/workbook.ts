d9d6dd4253d9d1d09377051f302d95c721b19cc5
seungmin <magin0auto@gmail.com>
2023-07-02 21:09:07 +0900

    :recycle:refactor: api 타입 추가 및 형태 일부 수정
    - 형태 수정: 인자값 형태 객체타입에서 일반타입으로 변경

--- src/api/workbook.ts
+++ src/api/workbook.ts
@@ -1,4 +1,5 @@
 import { instance } from 'api';
+import { FieldValues } from 'react-hook-form';
 import { WorkbookSetForm } from 'types/Form';
 import { Workbook } from 'types/workbookList';
 
@@ -11,14 +12,14 @@ export const getWorkbookList = async ({ page = 0, size = 8 }) => {
 };
 
 // 문제집 정보 요청
-export const getWorkbook = async ({ id }: any) => {
+export const getWorkbook = async (id: string) => {
     const response = await instance.get(`/api/workbook/${id}`);
     return response.data;
 };
 
 // 문제집 문제 요청
-export const getWorkbookQuestion = async ({ id, page = 0, size = 10 }: any) => {
-    const response = await instance.get(`/api/workbook/question/${id}?page=${page}&size=${size}`);
+export const getWorkbookQuestion = async (workbookPageId = '', page = 0, size = 10) => {
+    const response = await instance.get(`/api/workbook/question/${workbookPageId}?page=${page}&size=${size}`);
     return response.data;
 };
 
@@ -31,21 +32,21 @@ export const workbookSet = async (formData: WorkbookSetForm) => {
 };
 
 // 문제집 정보 수정
-export const updateWorkbook = async ({ formData }: any) => {
+export const updateWorkbook = async (formData: FieldValues) => {
     const response = await instance.post(`/api/workbook/update`, formData);
     return response;
 };
 
-// 문제집 정보 추가
-export const addWorkbookQuestion = async () => {
-    const response = await instance.post(`/api/workbook/question/add`);
+// 문제집 문제 추가
+export const addWorkbookQuestion = async (formData: FieldValues) => {
+    const response = await instance.post(`/api/workbook/question/add`, formData);
     return response;
 };
 
 /* -------- DELETE 요청 -------- */
 
 // 문제집 정보 삭제
-export const workbookQuestionDelete = async () => {
-    const response = await instance.delete(`/api/workbook/question/delete`);
+export const workbookQuestionDelete = async (formData: FieldValues) => {
+    const response = await instance.post(`/api/workbook/question/delete`, formData);
     return response;
 };
