daba0c2f083709fd3ec9eaf7f01c478ffd91b7b5
seungmin <magin0auto@gmail.com>
2023-07-05 21:14:27 +0900

    :sparkles:feat: 대회관련 서버에 넘길 데이터에 맞춰 변경 및 클릭시 바로 눌리는 현상때문에 debounce 추가

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,4 +1,4 @@
-const formatDate = (date: Date) => {
+const formatDate = (date: Date, options = { second: false }) => {
     // 일, 월, 년 추출
     const yyyy = date.getFullYear();
     const MM = String(date.getMonth() + 1).padStart(2, '0');
@@ -7,8 +7,9 @@ const formatDate = (date: Date) => {
     // 시, 분 추출
     const HH = String(date.getHours()).padStart(2, '0');
     const mm = String(date.getMinutes()).padStart(2, '0');
+    const ss = String(date.getSeconds()).padStart(2, '0');
 
-    return `${yyyy}-${MM}-${dd} ${HH}:${mm}`;
+    return `${yyyy}-${MM}-${dd} ${HH}:${mm}${options.second ? `:${ss}` : ''}`;
 };
 
 export default formatDate;
