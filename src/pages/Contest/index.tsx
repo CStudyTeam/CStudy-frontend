b926b04694a841449d2809285f0f881011b73f18
seungmin <magin0auto@gmail.com>
2023-07-07 16:16:53 +0900

    :sparkles:feat: 종료된 대회 리스트 보기 추가 (#5)

--- src/pages/Contest/index.tsx
+++ src/pages/Contest/index.tsx
@@ -7,20 +7,31 @@ import Pagination from 'components/ProblemSet/Pagination';
 import { useState, useCallback } from 'react';
 import useGetContestList from 'hooks/@query/contest/useGetContestList';
 import { Link } from 'react-router-dom';
+import { Filter } from 'pages/Board/style';
 
 const Contest = () => {
     const [page, setPage] = useState(0);
     const [query, setQuery] = useState('');
 
     const contestList = useGetContestList({ page, query });
+    const isActive = query === '/finish' ? 'active' : '';
 
     const handlePage = useCallback((page: number) => {
         setPage(page);
     }, []);
 
+    const handleToggle = () => {
+        setQuery(query === '' ? '/finish' : '');
+        setPage(0);
+    };
+
     return (
         <ContentContainer>
-            <ContentHeaderWrapper title="대회" adminLink="대회생성 페이지 이동" />
+            <ContentHeaderWrapper title="대회" adminLink="대회생성 페이지 이동">
+                <Filter className={isActive} onClick={handleToggle}>
+                    종료된 대회 보기
+                </Filter>
+            </ContentHeaderWrapper>
             <ContentBodyWrapper>
                 <Table colRate={['60%', '10%', '30%']} title={['대회명', '최대 인원수', '기간']}>
                     {contestList?.content?.map(({ id, title, startTime, endTime, participants }) => (
