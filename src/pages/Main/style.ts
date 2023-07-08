abc1b5b8ae187f286cf56c5b7fabada9ee98f400
seungmin <magin0auto@gmail.com>
2023-07-08 14:52:35 +0900

    :recycle:refactor: 3D 컴포넌트 파일 코드 정리 및 메인 배너 기본 백그라운드 설정(#3)

--- src/pages/Main/style.ts
+++ src/pages/Main/style.ts
@@ -1,3 +1,4 @@
+import { COLOR } from 'constants/Color';
 import styled from 'styled-components';
 
 export const MainWrapper = styled.main`
@@ -11,4 +12,5 @@ export const Banner = styled.div`
     width: 100%;
     height: 67rem;
     margin-top: 5rem;
+    background: linear-gradient(90deg, #e8e8e8 25%, #d8d8d8 50%, #e8e8e8 75%);
 `;
