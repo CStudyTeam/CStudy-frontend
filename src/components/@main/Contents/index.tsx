722d3da628375d92879dfa9e88d8fb454ea721c1
seungmin <magin0auto@gmail.com>
2023-07-13 20:59:57 +0900

    :art:style: 메인 문구 변경 (#3)

--- src/components/@main/Contents/index.tsx
+++ src/components/@main/Contents/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import * as Styled from './style';
-import Button from 'components/@shared/Button';
-import { Link } from 'react-router-dom';
 import StyleLink from 'components/@shared/StyleLink';
 
 const Contents = () => {
@@ -19,10 +17,10 @@ const Contents = () => {
                     </Styled.ContentsTitleImg>
                     <Styled.ContentsItem>
                         <Styled.ContentsTexts>
-                            <Styled.ContentsTextsTitle>책을 읽기 힘든 분들에게 추천해요!</Styled.ContentsTextsTitle>
+                            <Styled.ContentsTextsTitle>어렵게 생각하는 CS지식을 문의하세요!</Styled.ContentsTextsTitle>
                             <br />
                             <Styled.ContentsTextsDesc>
-                                안그래도 재미없는 전공! 한국어도 아닌 영어로 머리를 싸매고 힘들어하고 있으신가요?
+                                어렵게 생각하는 CS지식 혼자 공부하기 어려우신 가요?
                                 <br />
                                 쉽게 접근 할 수 있는 방법 없을까? 라고 고민해 본적 한번이라도 있으신가요?
                                 <br />
@@ -31,12 +29,12 @@ const Contents = () => {
                                     그래서 준비했습니다!
                                 </Styled.ContentsTextDescBold>
                                 <br />
-                                CSTUDY는 어려운 CS를 문제풀이 형식으로 필요한 지식을
+                                CSTUDY에 어렵게 생각하시는 CS지식을 문의하시면
                                 <br />
-                                더욱 쉽게 접근 하고 학습 할 수 있게 도와줍니다.
+                                퀴즈 형태로 더욱 쉽게 접근 하고 학습 할 수 있게 도와줍니다.
                             </Styled.ContentsTextsDesc>
-                            <StyleLink to="/problemset" className="xl navy style">
-                                문제 풀러 가기
+                            <StyleLink to="/board" className="xl navy style">
+                                게시판 보러 가기
                             </StyleLink>
                         </Styled.ContentsTexts>
                     </Styled.ContentsItem>
@@ -44,6 +42,47 @@ const Contents = () => {
             </Styled.Section>
             <Styled.Section>
                 <Styled.Wrapper>
+                    <Styled.ContentsItem>
+                        <Styled.ContentsTexts>
+                            <Styled.ContentsTextsTitle>
+                                대회에 참여해서 CS지식을 테스트해보세요!
+                            </Styled.ContentsTextsTitle>
+                            <br />
+                            <Styled.ContentsTextsDesc>
+                                안그래도 재미없는 전공! 한국어도 아닌 영어로 머리를 싸매고 힘들어하고 있으신가요?
+                                <br />
+                                그렇게 공부했는데 제대로 공부했는지 확신이 안 생기시나요?
+                                <br />
+                                <Styled.ContentsTextDescBold>
+                                    <br />
+                                    그래서 준비했습니다!
+                                </Styled.ContentsTextDescBold>
+                                <br />
+                                CSTUDY는 어려운 CS지식을 테스트 해주는 대회를 운영하고 있습니다.
+                                <br />
+                                대회를 통해 성장한 자신을 확인하고, 더욱 성장하세요!
+                            </Styled.ContentsTextsDesc>
+                            <StyleLink to="/contest" className="xl navy style">
+                                대회 보러 가기
+                            </StyleLink>
+                        </Styled.ContentsTexts>
+                    </Styled.ContentsItem>
+                    <Styled.ContentsTitleImg>
+                        <Styled.Title>
+                            CS지식 공부를 했는데
+                            <br />
+                            공부한것에 대한 확신이 없으신가요?
+                        </Styled.Title>
+                        <Styled.ContentsImg src="https://via.placeholder.com/543x320" alt="컨텐츠 이미지" />
+                    </Styled.ContentsTitleImg>
+                </Styled.Wrapper>
+            </Styled.Section>
+            <Styled.Section>
+                <Styled.Wrapper>
+                    <Styled.ContentsTitleImg>
+                        <Styled.Title>점수와 랭킹을 올려 랭커에 도전 하세요!</Styled.Title>
+                        <Styled.ContentsImg src="https://via.placeholder.com/543x320" alt="컨텐츠 이미지" />
+                    </Styled.ContentsTitleImg>
                     <Styled.ContentsItem>
                         <Styled.ContentsTexts>
                             <Styled.ContentsTextsTitle>선의의 경쟁을 통해 꾸준히 성장해요!</Styled.ContentsTextsTitle>
@@ -58,47 +97,8 @@ const Contents = () => {
                                 CSTUDY의 랭킹 시스템은 혼자 공부 하기 힘들고 어렵지만, 어려움을 딛고 같이 성장하기 위해
                                 노력하는 동료들을 확인하고 선의의 경쟁을 위해 준비했습니다.
                             </Styled.ContentsTextsDesc>
-                            <StyleLink to="/problemset" className="xl navy style">
+                            <StyleLink to="/membersranks" className="xl navy style">
                                 랭킹 확인 하기
-                            </StyleLink>
-                        </Styled.ContentsTexts>
-                    </Styled.ContentsItem>
-                    <Styled.ContentsTitleImg>
-                        <Styled.Title>점수와 랭킹을 올려 랭커에 도전 하세요!</Styled.Title>
-                        <Styled.ContentsImg src="https://via.placeholder.com/543x320" alt="컨텐츠 이미지" />
-                    </Styled.ContentsTitleImg>
-                </Styled.Wrapper>
-            </Styled.Section>
-            <Styled.Section>
-                <Styled.Wrapper>
-                    <Styled.ContentsTitleImg>
-                        <Styled.Title>
-                            CS공부 아직도 재미없는
-                            <br />
-                            해외 전공서적으로 공부하시나요?
-                        </Styled.Title>
-                        <Styled.ContentsImg src="https://via.placeholder.com/543x320" alt="컨텐츠 이미지" />
-                    </Styled.ContentsTitleImg>
-                    <Styled.ContentsItem>
-                        <Styled.ContentsTexts>
-                            <Styled.ContentsTextsTitle>책을 읽기 힘든 분들에게 추천해요!</Styled.ContentsTextsTitle>
-                            <br />
-                            <Styled.ContentsTextsDesc>
-                                안그래도 재미없는 전공! 한국어도 아닌 영어로 머리를 싸매고 힘들어하고 있으신가요?
-                                <br />
-                                쉽게 접근 할 수 있는 방법 없을까? 라고 고민해 본적 한번이라도 있으신가요?
-                                <br />
-                                <Styled.ContentsTextDescBold>
-                                    <br />
-                                    그래서 준비했습니다!
-                                </Styled.ContentsTextDescBold>
-                                <br />
-                                CSTUDY는 어려운 CS를 문제풀이 형식으로 필요한 지식을
-                                <br />
-                                더욱 쉽게 접근 하고 학습 할 수 있게 도와줍니다.
-                            </Styled.ContentsTextsDesc>
-                            <StyleLink to="/problemset" className="xl navy style">
-                                문제 풀러 가기
                             </StyleLink>
                         </Styled.ContentsTexts>
                     </Styled.ContentsItem>
